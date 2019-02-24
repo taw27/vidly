f6c92f4b591aa94df2e8e21b14fa4b3696652f61
TawshifTamjidAlam <tawshif27@gmail.com>
2019-02-23 23:07:11 -0800

    Add database fucntionality

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const express= require('express');// importing required modules
-const helmet= require('helmet');
-const config= require('config');
+const express = require('express');// importing required modules
+const helmet = require('helmet');
+const config = require('config');
 const process= require('process');
-const app= express();
+const app = express();
+const mongoose = require('mongoose');
 const genresRouter= require('./routes/genres')
 const port= process.env.PORT || config.get("Genres.port");
 
 app.use(express.json());//middlewares
 app.use(helmet());
+mongoose.connect('mongodb://localhost/playground', {useNewUrlParser: true})
+  .then(()=> console.log('Connected to MongoDB...'))
+  .catch(err=> console.error('could not connect to the database', err));
+  
 app.use('/api/genres', genresRouter);
 
 app.listen(port);
--- routes/genres.js
+++ routes/genres.js
@@ -1,19 +1,14 @@
 const express= require('express');
 const router=express.Router();
 const Joi= require('joi');
-
-const movieGenres=[
-    {id: 1, name: 'action'}, // dummy data
-    {id: 2, name: 'comedy'},
-    {id: 3, name: 'thriller'},
-];
+const Genre = require('../models/Genre');
 
 router.get('/', (req, res)=>{
-    return res.send(movieGenres);
+    return res.send(getGenrebyName(''));
 });
 
-router.get('/:genreId', (req, res)=>{
-    const genre= getGenrebyId(req.params.genreId);
+router.get('/:genreName', (req, res)=>{
+    const genre= getGenrebyName(req.params.genreName);
     return genre? res.send(genre): res.status(404).send("Genre Not Found");
 });
 
@@ -24,37 +19,51 @@ router.post('/',(req, res)=>{
 
     if(getGenrebyName(value.name)) return res.status(400).send('Genre already exists');
     else {
-    const newGenre={id: movieGenres.length+1, name: value.name}
-    movieGenres.push(newGenre);
-    return res.status(200).send(newGenre);
+    const newGenre={ name: value.name, movies: value.movies}
+    return res.status(200).send(insertGenre(newGenre));
     }
 });
 
-router.put('/:genreId',(req, res)=>{
+router.put('/:genreName',(req, res)=>{
     const {error, value} = validateGenre(req.body);
 
     if(error) return res.status(400).send(error.details[0].message);
 
-    const genre= getGenrebyId(req.params.genreId);
+    const genre= getGenrebyName(req.params.genreName);
     if(!genre) return res.status(400).send('Genre does not exist');
 
-    genre.name= value.name;
+   updateGenre(value)
 
     return  res.status(200).send(genre);
 });
 
-
-/** Returns the genre if it exists, otherwise returns undefined */
-function getGenrebyId(genreId) {
-    return movieGenres.find((genre)=>{
-    return genre.id===parseInt(genreId);
-    });
+async function getGenrebyName(genreName) {
+    try{
+    let result = await Genre
+    .find({ name: genreName.toLowerCase() });
+    }catch(err){
+        return null;
+    }
+    return result; 
 }
 
-function getGenrebyName(genreName) {
-    return movieGenres.find((genre)=>{
-    return genre.name===genreName;
-    });
+async function updateGenre(genre){
+
+    try{
+        const genre = getGenrebyName(genre.name);
+    return await insertGenre(genre);
+    }catch(err){
+        return err.errors['name'].message;
+    }
+}
+
+async function insertGenre(genre){
+
+    try{
+    return await new Genre(genre).save();
+    }catch(err){
+        return err.errors['name'].message;
+    }
 }
 
 function validateGenre(genreName){
